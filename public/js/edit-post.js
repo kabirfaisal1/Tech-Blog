d50279f30e91bd5be27bd74985524438273c9b8d
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-04-06 20:25:39 -0400

    working on edit

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -25,4 +25,4 @@ async function editFormHandler(event) {
       }
   }
   
-  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  document.querySelector('#edit_post_form').addEventListener('submit', editFormHandler);
